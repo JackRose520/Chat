94b42a102f77141d11a05863a83641926f5b1fce
Jack <jack820075@gmail.com>
2017-02-03 15:15:33 +0800

    Openshift

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){      //竊聽3000 port
+  console.log('listening on *:3000'); //印出listen訊息
+});
